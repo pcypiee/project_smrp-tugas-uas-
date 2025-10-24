61406ddb4aed5315fec1ec1795d4cda82f5cc9eb
deviival <apriantidevi309@gmail.com>
2025-10-24 17:31:59 +0700

    mcr GoodReceipts

---
+++ database/migrations/2025_10_24_005934_create_goods_receipts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('goods_receipts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('po_id');
+            $table->unsignedBigInteger('lokasi_penerima_id');
+            $table->date('tanggal_terima');
+            $table->unsignedBigInteger('penerima_pegawai_id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('goods_receipts');
+    }
+};
